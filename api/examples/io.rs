d6f3f3a990ca68794b7883aefca23e882293dca1
Ho Kim <ho.kim@ulagbulag.io>
2022-05-06 09:19:06 +0000

    Apply CURD pattern in io example

--- api/examples/io.rs
+++ api/examples/io.rs
@@ -18,19 +18,32 @@ pub struct MyData {
 async fn main() -> Result<()> {
     let client = IpdisClient::infer()?;
 
-    let data = MyData {
+    let mut data = MyData {
         name: "Alice".to_string(),
         age: 42,
     };
 
-    // PUT
-    let path = client.put_permanent(&data).await?;
+    // CREATE
+    let path_create = client.put_permanent(&data).await?;
 
-    // GET
+    // UPDATE (identity)
+    let path_update_identity = client.put_permanent(&data).await?;
+    assert_eq!(&path_create, &path_update_identity); // SAME Path
+
+    // UPDATE (changed)
+    data.name = "Bob".to_string();
+
+    let path_update_changed = client.put_permanent(&data).await?;
+    assert_ne!(&path_create, &path_update_changed); // CHANGED Path
+
+    let path = path_update_changed;
+
+    // READ
     let data_from_storage: MyData = client.get(&path).await?;
     assert_eq!(&data, &data_from_storage);
 
     // DELETE
+    let () = client.delete(&path_create).await?;
     let () = client.delete(&path).await?;
 
     // data is not exist after DELETE
